26c6a98302159db67de8355946baea89e65f0bb0
Thomas Leis <tjleis25@gmail.com>
2018-06-14 08:54:52 -0400

    completed version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,37 +17,24 @@ var totalScore = 0;
 //====================================================================================================
 
 //startGame()
-//start and restart the game
 
 function startGame(min, max) {
-       
+    
     console.log("Computer Choice: " + computerChoice);
-
+    
     blue; 
     console.log("Blue Crystal: " + blue);
-
+    
     green; 
     console.log("Green Crystal: " + green);
     
     red; 
     console.log("Red Crystal: " + red);
-
+    
     yellow; 
     console.log("Yellow Crystal: " + yellow);
     
 }
-
-// function nextGame() {
-
-//     var computerChoice = Math.floor(Math.random() * (120-19) + 19);
-//     var yellow = Math.floor(Math.random() * (12-1) + 1);
-//     var red = Math.floor(Math.random() * (11) + 1);
-//     var green = Math.floor(Math.random() * (11) + 1);
-//     var blue = Math.floor(Math.random() * (11) + 1);
-    
-// }
-
-
 
 //Create an event listener
 //=======================================================
@@ -68,10 +55,10 @@ $("#yellow-crystal").on("click", function() {
         $("#total-score").text(totalScore);
         $("#score").text(computerChoice);
         newNumber();        
-
+        
         
     } 
-        else if (totalScore > computerChoice) {
+    else if (totalScore > computerChoice) {
         alert ("You Lose!");
         losses++;
         console.log("Losses: " + losses);
@@ -104,7 +91,7 @@ $("#red-crystal").on("click", function() {
         
         
     } 
-        else if (totalScore > computerChoice) {
+    else if (totalScore > computerChoice) {
         alert ("You Lose!");
         losses++;
         console.log("Losses: " + losses);
@@ -135,8 +122,8 @@ $("#green-crystal").on("click", function() {
         $("#total-score").text(totalScore); 
         $("#score").text(computerChoice);
         newNumber();
-                        
-
+        
+        
     } else if (totalScore > computerChoice) {
         alert ("You Lose!");
         losses++;
@@ -146,7 +133,7 @@ $("#green-crystal").on("click", function() {
         $("#total-score").text(totalScore); 
         $("#score").text(computerChoice);
         newNumber();       
-                 
+        
     }
     
 })
@@ -167,8 +154,8 @@ $("#blue-crystal").on("click", function() {
         $("#total-score").text(totalScore); 
         $("#score").text(computerChoice); 
         newNumber();     
-         
-
+        
+        
     } else if (totalScore > computerChoice) {
         alert ("You Lose!");
         losses++;
@@ -178,24 +165,28 @@ $("#blue-crystal").on("click", function() {
         $("#total-score").text(totalScore); 
         $("#score").text(computerChoice);
         newNumber();     
-       
+        
         
     }
     
 })
 
 function newNumber() {
-
+    
     computerChoice = Math.floor(Math.random() * (120-19) + 19);
     $("#score").text(computerChoice); 
-
+    yellow = Math.floor(Math.random() * (12-1) + 1);
+    red = Math.floor(Math.random() * (11) + 1);
+    green = Math.floor(Math.random() * (11) + 1);
+    blue = Math.floor(Math.random() * (11) + 1);
+    
 }
 
 //text to html
 //====================================================
 
 $("#score").text(computerChoice); 
- 
+
 
 
 
@@ -204,7 +195,7 @@ $("#score").text(computerChoice);
 
 $(document).ready(function() {
     
-startGame(19,120); 
+    startGame(19,120); 
     
     
 })
